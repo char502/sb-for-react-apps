0b5121a9bfefaef293985df3d1df210e8c56c00c
charlotte.ellwood <charlotte.ellwood@brightpearl.com>
2022-10-09 16:31:53 +0100

    adding figma to storybook

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { DecoratorFn } from '@storybook/react'
+import { withDesign } from 'storybook-addon-designs'
 import { GlobalStyle } from '../src/styles/GlobalStyle'
-import { lightTheme } from '../src/styles/theme'
+import { lightTheme, darkTheme } from '../src/styles/theme'
 
-export const withTheme: DecoratorFn = (StoryFn) => {
+export const withTheme: DecoratorFn = (StoryFn, context) => {
+  // check if theme exists, if not, fall back to the globals
+  const theme = context.parameters.theme || context.globals.theme
+  const storyTheme = theme === 'dark' ? darkTheme : lightTheme
+
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={storyTheme}>
       <GlobalStyle />
       <StoryFn />
     </ThemeProvider>
   )
 }
 
-export const globalDecorators = [withTheme]
+export const globalDecorators = [withTheme, withDesign]
--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -8,8 +8,26 @@ export default {
   args: {
     text: 'Comfort Food',
   },
+  parameters: {
+    design: {
+      type: 'figspec',
+      url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=1906%3A3469',
+    },
+  },
 } as ComponentMeta<typeof Badge>
 
 const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />
 
 export const Default = Template.bind({})
+// Link a specific design to a version of badge
+// Default.parameters = {
+//   design: {
+//     type: 'figma',
+//     url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=1128%3A3451',
+//   },
+// }
+
+export const DarkTheme = Template.bind({})
+DarkTheme.parameters = {
+  theme: 'dark',
+}
